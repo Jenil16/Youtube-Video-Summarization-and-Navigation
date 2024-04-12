1c4fdf38a10f6d0c87c5f9fd044e4d5fd3ec26bf
Vedant Patel <110patelvedant@gmail.com>
2024-04-12 11:22:45 +0530

    added gradient & about section

--- app.py
+++ app.py
@@ -89,10 +89,17 @@ subtitles = None
 def index():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
 
 @app.route('/forgot', methods=['GET', 'POST'])
